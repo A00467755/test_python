ef1ab34066c4ef3ff7a6d7fdb27675449035f54e
Kin Wa, Chan <Kin.Wa.Chan@smu.ca>
2023-04-13 18:39:44 -0300

    test2

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from joblib import load
+from sklearn import tree
 
-st.title("vxxvxvx")
+st.title("Deploying the model")
+LABELS = ["setosa","versicolor","virgnica"]
+
+clf = load("DT.joblib")
+
+sp_l = st.slider("sepal length (cm)", min_value = 0, max_value = 10)
+sp_w = st.slider("sepal width (cm)", min_value = 0, max_value = 10)
+
+pe_l = st.slider("petal length (cm)", min_value = 0, max_value = 10)
+pe_w = st.slider("petal width (cm)", min_value = 0, max_value = 10)
+
+prediction = clf.predict([[sp_l,sp_w,pe_l,pe_w]])
+
+st.write(LABELS[prediction])
+
+
+
+
